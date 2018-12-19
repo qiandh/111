941213ba420e1505a83457994936ecf7cad728fd
钱东航 <792586254@qq.com>
2018-12-19 21:32:41 +0800

    2222

--- js/index.js
+++ js/index.js
@@ -18,19 +18,19 @@ $(function () {
         $(".cover").css({ display: 'block' })
         //让当前li添加类样式
         if (falg) {
-            $(".spone").css({ display: 'block' });
-            $(".sptwo").css({ display: 'none' });
+            $(".spone").css({ display: 'none' });
+            $(".sptwo").css({ display: 'block' });
             falg = false
             //播放 falg = false
         } else {
-            $(".spone").css({ display: 'none' });
-            $(".sptwo").css({ display: 'block' });
+            $(".spone").css({ display: 'block' });
+            $(".sptwo").css({ display: 'none' });
             falg = true
         }
     })
     $('html').on('touchstart', function (event) {
         //如果暂停了就不播放了
-        audio.play()
+        
         // console.log(event.target)
         // console.log($(event.target).parents('.musics'))
         // console.log($(event.target).hasClass('musics'))
@@ -41,8 +41,10 @@ $(function () {
         //只要&&两边有一个是true则就是触碰到了盒子 所以不隐藏
         if ($(event.target).parents('.musics').length == 0 && !$(event.target).hasClass('musics')) {
             // console.log($(event.target))
+            audio.play();
             // console.log($(event.target).parents('.cover').length)
             $('.cover').hide()
+            console.log(111)
         }
         $(".onebtmhi").delay(2000).show(2000);
     });
@@ -53,7 +55,7 @@ $(function () {
         scrollingSpeed: 1000,
     });
     //载入页面
-    $('#loading').delay(3000).fadeOut(1500, function () {
+    $('#loading').delay(3).fadeOut(1500, function () {
         bf();
     });
     //汉堡
@@ -69,7 +71,6 @@ $(function () {
     //点击a 隐藏面板 添加样式
     $("#menu>li").on("click", "a", function () {
         $(this).parent().siblings("li").find("a").removeClass("aactive")
-        console.log(this)
         $(this).addClass("aactive")
         //修改span长度
         $(this).parent().siblings("li").find("span").removeClass("span-active")
@@ -77,5 +78,6 @@ $(function () {
         $(".zhezhao").hide();
         $(".navigation").hide();
         $("#menu").hide();
+        $(".hamburger").toggleClass("is-active");
     })
 })
